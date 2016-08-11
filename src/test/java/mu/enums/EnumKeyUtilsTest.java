f0560eaf6c54506b1291f374999643a22c987c82
oshai <ohadshai@gmail.com>
2016-08-11 14:02:14 +0300

    formatting

--- src/test/java/mu/enums/EnumKeyUtilsTest.java
+++ src/test/java/mu/enums/EnumKeyUtilsTest.java
@@ -11,57 +11,52 @@ import java.util.Map;
 import static mu.enums.EnumKeyUtils.buildKeysMap;
 import static org.junit.Assert.assertEquals;
 
-public class EnumKeyUtilsTest
-{
-	private enum EnumEmptyTest implements IUniqueKeyEnum<Integer>
-	{
-		;
+public class EnumKeyUtilsTest {
 
-		@Override
-		public Integer getKey()
-		{
-			return null;
-		}
-	}
+    private enum EnumEmptyTest implements IUniqueKeyEnum<Integer> {
+        ;
 
-	private enum EnumTest implements IUniqueKeyEnum<Integer>
-	{
-		A(1),
-		B(2);
-		
-		private Integer m_iKey;
-		
-		EnumTest(int iKey)
-		{
-			m_iKey = iKey;
-		}
-		
-		@Override
-		public Integer getKey()
-		{
-			return m_iKey;
-		}
-	}
-	
-	@Before
-	public void setUp() throws Exception
-	{
-		EnumKeyChecker.check(EnumTest.class);
-	}
-	
-	@After
-	public void tearDown() throws Exception
-	{
-	}
-	
-	@Test
-	public void testBuildKeysMap()
-	{
-		assertEquals(Collections.emptyMap(), buildKeysMap(EnumEmptyTest.class));
-		Map testMap = new HashMap<Integer,EnumTest>(){{put(1,EnumTest.A); put(2, EnumTest.B);  }} ;
+        @Override
+        public Integer getKey() {
+            return null;
+        }
+    }
+
+    private enum EnumTest implements IUniqueKeyEnum<Integer> {
+        A(1),
+        B(2);
+
+        private Integer m_iKey;
+
+        EnumTest(int iKey) {
+            m_iKey = iKey;
+        }
+
+        @Override
+        public Integer getKey() {
+            return m_iKey;
+        }
+    }
+
+    @Before
+    public void setUp() throws Exception {
+        EnumKeyChecker.check(EnumTest.class);
+    }
+
+    @After
+    public void tearDown() throws Exception {
+    }
+
+    @Test
+    public void testBuildKeysMap() {
+        assertEquals(Collections.emptyMap(), buildKeysMap(EnumEmptyTest.class));
+        Map testMap = new HashMap<Integer, EnumTest>() {{
+            put(1, EnumTest.A);
+            put(2, EnumTest.B);
+        }};
 
 
-		assertEquals(testMap, buildKeysMap(EnumTest.class));
-	}
-	
+        assertEquals(testMap, buildKeysMap(EnumTest.class));
+    }
+
 }
--- src/test/java/mu/enums/EnumNameUtilsTest.java
+++ src/test/java/mu/enums/EnumNameUtilsTest.java
@@ -11,76 +11,71 @@ import static mu.enums.EnumNameUtils.fromNameIgnoreCase;
 import static org.junit.Assert.assertEquals;
 
 
-public class EnumNameUtilsTest 
-{
-	private enum EnumTest implements INamedEnum
-	{
-		A("A"),
-		ANOTHER_A("A"),
-		B("B"),
-		b("b");
-		
-		private String m_sName;
-		EnumTest(String sName)
-		{
+public class EnumNameUtilsTest {
+    private enum EnumTest implements INamedEnum {
+        A("A"),
+        ANOTHER_A("A"),
+        B("B"),
+        b("b");
 
-					m_sName = sName;
-		}
-		@Override
-		public String getName() 
-		{
-			return m_sName;
-		}
-	}
-	@Before
-	public void setUp() throws Exception 
-	{
-	}
+        private String m_sName;
 
-	@After
-	public void tearDown() throws Exception 
-	{
-	}
+        EnumTest(String sName) {
 
-	@Test
-	public void testFromName() 
-	{
-		
-		assertEquals(null, fromName("b", EnumSet.of(EnumTest.B,EnumTest.A)));
-		assertEquals(null, fromName("B", EnumSet.of(EnumTest.b,EnumTest.A)));
-		assertEquals(EnumTest.b, fromName("b", EnumSet.of(EnumTest.b,EnumTest.B,EnumTest.A)));
-		assertEquals(EnumTest.B, fromName("B", EnumSet.of(EnumTest.B,EnumTest.A,EnumTest.b)));
-		assertEquals(null, fromNameIgnoreCase("C", EnumSet.of(EnumTest.b,EnumTest.A)));
-		assertEquals(null, fromNameIgnoreCase("C", EnumSet.noneOf(EnumTest.class)));
-		assertEquals(null, fromNameIgnoreCase("b", EnumSet.noneOf(EnumTest.class)));
-	}
+            m_sName = sName;
+        }
 
-	@Test
-	public void testFromNameIgnoreCase()
-{
-		assertEquals(EnumTest.B, fromNameIgnoreCase("B", EnumSet.of(EnumTest.B,EnumTest.A)));
-		assertEquals(EnumTest.B, fromNameIgnoreCase("b", EnumSet.of(EnumTest.B,EnumTest.A)));
-		assertEquals(EnumTest.b, fromNameIgnoreCase("b", EnumSet.of(EnumTest.b,EnumTest.A)));
-		assertEquals(EnumTest.b, fromNameIgnoreCase("B", EnumSet.of(EnumTest.b,EnumTest.A)));
-		assertEquals(null, fromNameIgnoreCase("C", EnumSet.of(EnumTest.b,EnumTest.A)));
-		assertEquals(null, fromNameIgnoreCase("C", EnumSet.noneOf(EnumTest.class)));
-		assertEquals(null, fromNameIgnoreCase("b", EnumSet.noneOf(EnumTest.class)));
-	}
+        @Override
+        public String getName() {
+            return m_sName;
+        }
+    }
 
-	@Test
-	public void testCheck() 
-	{
-		EnumNameUtils.check(EnumSet.of(EnumTest.B,EnumTest.A));
-		EnumNameUtils.check(EnumSet.of(EnumTest.B,EnumTest.A,EnumTest.b));
-		EnumNameUtils.check(EnumSet.noneOf(EnumTest.class));
-	}
+    @Before
+    public void setUp() throws Exception {
+    }
 
-	@Test(expected=IllegalArgumentException.class)
-	public void testCheckError1() {
-		EnumNameUtils.check(EnumSet.of(EnumTest.ANOTHER_A, EnumTest.A));
-	}
-	@Test(expected=IllegalArgumentException.class)
-	public void testCheckError2() {
-		EnumNameUtils.check(EnumSet.allOf(EnumTest.class));
-	}
+    @After
+    public void tearDown() throws Exception {
+    }
+
+    @Test
+    public void testFromName() {
+
+        assertEquals(null, fromName("b", EnumSet.of(EnumTest.B, EnumTest.A)));
+        assertEquals(null, fromName("B", EnumSet.of(EnumTest.b, EnumTest.A)));
+        assertEquals(EnumTest.b, fromName("b", EnumSet.of(EnumTest.b, EnumTest.B, EnumTest.A)));
+        assertEquals(EnumTest.B, fromName("B", EnumSet.of(EnumTest.B, EnumTest.A, EnumTest.b)));
+        assertEquals(null, fromNameIgnoreCase("C", EnumSet.of(EnumTest.b, EnumTest.A)));
+        assertEquals(null, fromNameIgnoreCase("C", EnumSet.noneOf(EnumTest.class)));
+        assertEquals(null, fromNameIgnoreCase("b", EnumSet.noneOf(EnumTest.class)));
+    }
+
+    @Test
+    public void testFromNameIgnoreCase() {
+        assertEquals(EnumTest.B, fromNameIgnoreCase("B", EnumSet.of(EnumTest.B, EnumTest.A)));
+        assertEquals(EnumTest.B, fromNameIgnoreCase("b", EnumSet.of(EnumTest.B, EnumTest.A)));
+        assertEquals(EnumTest.b, fromNameIgnoreCase("b", EnumSet.of(EnumTest.b, EnumTest.A)));
+        assertEquals(EnumTest.b, fromNameIgnoreCase("B", EnumSet.of(EnumTest.b, EnumTest.A)));
+        assertEquals(null, fromNameIgnoreCase("C", EnumSet.of(EnumTest.b, EnumTest.A)));
+        assertEquals(null, fromNameIgnoreCase("C", EnumSet.noneOf(EnumTest.class)));
+        assertEquals(null, fromNameIgnoreCase("b", EnumSet.noneOf(EnumTest.class)));
+    }
+
+    @Test
+    public void testCheck() {
+        EnumNameUtils.check(EnumSet.of(EnumTest.B, EnumTest.A));
+        EnumNameUtils.check(EnumSet.of(EnumTest.B, EnumTest.A, EnumTest.b));
+        EnumNameUtils.check(EnumSet.noneOf(EnumTest.class));
+    }
+
+    @Test(expected = IllegalArgumentException.class)
+    public void testCheckError1() {
+        EnumNameUtils.check(EnumSet.of(EnumTest.ANOTHER_A, EnumTest.A));
+    }
+
+    @Test(expected = IllegalArgumentException.class)
+    public void testCheckError2() {
+        EnumNameUtils.check(EnumSet.allOf(EnumTest.class));
+    }
 }
