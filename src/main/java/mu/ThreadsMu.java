e69a64d93d7d372296edcc464f41ee106dd5bfe4
Ohad Shai <oshai@outbrain.com>
2017-10-02 11:44:38 +0300

    add createThreadPoolExecutor

--- src/main/java/mu/ThreadsMu.java
+++ src/main/java/mu/ThreadsMu.java
@@ -1,5 +1,10 @@
 package mu;
 
+import com.google.common.util.concurrent.ThreadFactoryBuilder;
+
+import java.util.concurrent.BlockingQueue;
+import java.util.concurrent.LinkedBlockingQueue;
+import java.util.concurrent.ThreadPoolExecutor;
 import java.util.concurrent.TimeUnit;
 
 public class ThreadsMu {
@@ -10,5 +15,18 @@ public class ThreadsMu {
     } catch (InterruptedException e) {
       throw ExceptionsMu.handleInterruptedException(e);
     }
+  }
+
+  public static ThreadPoolExecutor createThreadPoolExecutor(int threadPoolSize, int workQueueSize, String poolName) {
+    BlockingQueue<Runnable> workQueue = new LinkedBlockingQueue<>(workQueueSize);
+    ThreadPoolExecutor threadPoolExecutor = new ThreadPoolExecutor(
+      threadPoolSize,
+      threadPoolSize,
+      1,
+      TimeUnit.MINUTES,
+      workQueue,
+      new ThreadFactoryBuilder().setNameFormat(poolName + "-%d").build());
+    threadPoolExecutor.allowCoreThreadTimeOut(true);
+    return threadPoolExecutor;
   }
 }
--- src/test/java/mu/ThreadsMuTest.java
+++ src/test/java/mu/ThreadsMuTest.java
@@ -3,6 +3,7 @@ package mu;
 import mu.exceptions.InterruptedError;
 import org.junit.Test;
 
+import java.util.concurrent.ThreadPoolExecutor;
 import java.util.concurrent.TimeUnit;
 
 import static org.junit.Assert.*;
@@ -13,6 +14,21 @@ public class ThreadsMuTest {
   public void sleep() throws Exception {
     Thread.currentThread().interrupt();
     ThreadsMu.sleep(10, TimeUnit.MILLISECONDS);
+  }
+
+  @Test
+  public void test_createThreadPoolExecutor() throws Exception {
+    int workQueueSize = 1_000_000;
+    int threadPoolSize = 10;
+    ThreadPoolExecutor pool = ThreadsMu.createThreadPoolExecutor(threadPoolSize, workQueueSize, "myPool");
+    assertEquals(0, pool.getPoolSize());
+    assertEquals(threadPoolSize, pool.getCorePoolSize());
+    assertEquals(threadPoolSize, pool.getMaximumPoolSize());
+    assertEquals(0, pool.getCompletedTaskCount());
+    assertEquals(0, pool.getQueue().size());
+    assertEquals(workQueueSize, pool.getQueue().remainingCapacity());
+    assertEquals(false, pool.isShutdown());
+    pool.shutdown();
   }
 
 }
